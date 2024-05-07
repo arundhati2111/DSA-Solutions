71d40a8b95d7466007329287245e30e8ce257c9a
Arundhati2111 <puriarundhati@gmail.com>
2024-05-07 12:37:39 +0530

    insertion in singly linked list

---
+++ LinkedList2.cpp/length.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+
+        Node(){ //default ctor
+            this->next=NULL;
+            cout<<"Inside defuault ctor"<<endl;
+        }
+
+        Node(int data){
+            this->data=data;
+            this->next=NULL;
+            cout<<"Inside parameterized ctor"<<endl;
+        }
+};
+
+void printLL(Node* head){
+    Node* temp=head;
+
+    while(temp != NULL){
+        cout<<temp->data<<"-> ";
+        temp=temp->next;
+    }
+}
+
+int getLength(Node *head){
+    Node* temp=head;
+    int length=0;
+    while(temp!=NULL){
+        length++;
+        temp=temp->next;
+    }
+    return length;
+}
+
+int main(){
+    // Node n; STATIC ALLOCATION
+    // DYNAMIC ALLOCATION
+    Node* first=new Node(10);
+    Node* second=new Node(20);
+    Node* third=new Node(30);
+    Node* fourth=new Node(40);
+    Node* fifth=new Node(50);
+
+    first->next=second;
+    second->next=third;
+    third->next=fourth;
+    fourth->next=fifth;
+
+    Node* head=first;
+    printLL(head);
+    cout<<endl;
+    cout<<"Length of Linked List is: "<<getLength(head)<<endl;
+    return 0;
+}
