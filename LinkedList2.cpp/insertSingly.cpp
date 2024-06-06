7e52304d73b80e052e4fb2163839c99e72f57475
Arundhati2111 <puriarundhati@gmail.com>
2024-06-06 22:16:06 +0530

    Updated linkedlist insertion

--- LinkedList2.cpp/insertSingly.cpp
+++ LinkedList2.cpp/insertSingly.cpp
@@ -25,6 +25,7 @@ void printLL(Node* head){
         cout<<temp->data<<"-> ";
         temp=temp->next;
     }
+    cout<<endl;
 }
 
 int getLength(Node *head){
@@ -37,39 +38,97 @@ int getLength(Node *head){
     return length;
 }
 
-void insertAtHead(Node* &head,int data){
+void insertAtHead(Node* &head,Node* &tail,int data){
         Node* newNode=new Node(data);
         if(head==NULL){
             head=newNode;
+            tail=newNode;
         }
         else{
             newNode->next=head;
             head=newNode;
+            
         }
         
 }
 
+void insertAtTail(Node* &head,Node* &tail,int data){
+    Node* newNode=new Node(data);
+    if(head==NULL){
+        head=newNode;
+        tail=newNode;
+    }
+    else{
+        tail->next=newNode;
+        tail=newNode;
+    }
+}
+
+void createTail(Node* &head,Node* &tail){
+    Node* temp=head;
+    while(temp->next!=NULL){
+
+        temp=temp->next;
+    }
+    tail=temp;
+}
+
+
+void insertAtPosition(Node* &head, Node* &tail,int data, int position){
+    // if(position<1){
+    //     cout<<"Cannot insert, Please enter a valid position."<<endl;
+    //     return;
+    // }
+    int length=getLength(head);
+    // if(position>length){
+    //     cout<<"Cannot insert, Please enter a valid position."<<endl;
+    //     return;
+    // }
+
+    if(position<=1){
+        insertAtHead(head,tail,data);
+    }
+
+    else if(position > length){
+        insertAtTail(head,tail,data);
+    }
+
+    else{
+        // insert in between linked list 
+        // step1: create a node
+        Node *newNode=new Node(data);
+        // step 2: traverse prev and curr to position
+        Node *prev=NULL;
+        Node *curr=head;
+        while(position != 1){
+            prev=curr;
+            curr=curr->next;
+            position--;
+        }
+        // attach prev to new node
+        prev->next=newNode;
+        // attach new node to current
+        newNode->next=curr;
+    }
+}
+
 int main(){
-    // Node n; STATIC ALLOCATION
-    // DYNAMIC ALLOCATION
-    // Node* first=new Node(10);
-    // Node* second=new Node(20);
-    // Node* third=new Node(30);
-    // Node* fourth=new Node(40);
-    // Node* fifth=new Node(50);
-
-    // first->next=second;
-    // second->next=third;
-    // third->next=fourth;
-    // fourth->next=fifth;
-
-    // Node* head=first;
-    // printLL(head);
-    // cout<<endl;
-    // cout<<"Length of Linked List is: "<<getLength(head)<<endl;
     Node* head=NULL;
-    insertAtHead(head,60);
+    Node* tail=NULL;
+    // insertAtHead(head,tail,60);
+    // printLL(head);
+    // insertAtTail(head,tail,500);
+    // printLL(head);
+    insertAtTail(head,tail,10);
+    insertAtTail(head,tail,20);
+    insertAtTail(head,tail,30);
+    insertAtTail(head,tail,50);
+    printLL(head);
+    // insertAtPosition(head, tail, 40, 0); Cannot insert, Please enter a valid position.
+    // insertAtPosition(head, tail, 40, 6);  Cannot insert, Please enter a valid position.
+    insertAtPosition(head, tail, 40, 50);
     printLL(head);
     return 0;
+
 
 }
